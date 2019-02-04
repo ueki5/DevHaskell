4af5b7f0e4ca1cb64f6ac1c3f4c6348b82cfc098
ueki5 <ueki005@gmail.com>
2019-02-05 00:38:41 +0900

    a

--- src/Sample02.hs
+++ src/Sample02.hs
@@ -2,8 +2,10 @@ module Sample02 where
 
 type Bit = Int
 bin2int::[Bit] -> Int
-bin2int bits = sum [w * b | (w, b) <- zip weights (reverse bits)]
-    where weights = iterate (*2) 1
+bin2int = bin2int' . reverse where
+    bin2int' bits = sum [w * b | (w, b) <- zip weights bits]
+    weights = iterate (*2) 1
 int2bin::Int -> [Bit]
-int2bin 0 = []
-int2bin n = mod n 2 : int2bin (n `div` 2)
+int2bin = reverse . int2bin' where
+    int2bin' 0 = []
+    int2bin' n = mod n 2 : int2bin' (n `div` 2)
